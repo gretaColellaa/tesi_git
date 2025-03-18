310e40ead4279a88fe0891870485f90e97f2eb52
greta colella <gretacolella296@gmail.com>
2025-03-18 18:55:37 +0100

    creazione richiesta.html

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def login():
 def signup():
     return render_template('signup.html')
 
+@app.route('/richiesta')
+def richiesta():
+    return render_template('richiesta.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
